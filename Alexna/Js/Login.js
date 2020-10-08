f9160a77d64db99286eeb52dbd38b05bf68bad0b
Rafael Lorenzo <rafael.lorenzo117@comunidadunir.net>
2020-10-08 22:49:16 +0200

    Montaje inicial y pagina de login

---
+++ Alexna/Js/Login.js
@@ -0,0 +1,30 @@
+﻿let Login = (function () {
+    "use strict";
+
+    function Inicio() {
+        // Click events
+        $('#btnLogin').on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            let username = $('#username').val();
+            let password = $('#password').val();
+
+            if (!username) {
+                //TODO: No se ha introducido el usuario.
+            } else if (!password) {
+                //TODO: No se ha introducido la contraseña.
+            } else {
+                //TODO: Validar usuario.
+            }
+        });
+    }
+
+    function pValidar(usuario, contrasena) {
+        //TODO: Comprobar usuario y contraseña.
+    }
+
+    return {
+        Inicio: Inicio
+    }
+})();
